20dbf765f2c3e47fb528444289642e0c4c8d849a
nanguaguag <2694536263@qq.com>
2024-11-09 14:28:46 +0000

    add ResNet18

--- cnn.py
+++ cnn.py
@@ -11,16 +11,20 @@ from torch.utils.data import DataLoader, TensorDataset, random_split
 from datetime import datetime
 from tqdm import tqdm
 
+
 class EfficientNetB0Model(nn.Module):
     def __init__(self, num_classes):
         super(EfficientNetB0Model, self).__init__()
         # 加载预训练的EfficientNet-B0模型
-        self.model = models.efficientnet_b0(weights = "EfficientNet_B0_Weights.DEFAULT")
+        self.model = models.efficientnet_b0(weights="EfficientNet_B0_Weights.DEFAULT")
         # 替换最后一层以适应我们的分类任务
-        self.model.classifier[1] = nn.Linear(self.model.classifier[1].in_features, num_classes)
+        self.model.classifier[1] = nn.Linear(
+            self.model.classifier[1].in_features, num_classes
+        )
 
     def forward(self, x):
         return self.model(x)
+
 
 class SimpleCNN(nn.Module):
     def __init__(self):
@@ -39,10 +43,12 @@ class SimpleCNN(nn.Module):
         x = self.fc2(x)
         return x
 
+
 def unpickle(file):
     import pickle
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
+
+    with open(file, "rb") as fo:
+        dict = pickle.load(fo, encoding="bytes")
     return dict
 
 
@@ -71,7 +77,9 @@ def train_model(model, train_loader, test_loader, criterion, optimizer, num_epoc
         correct = 0
         total = 0
 
-        for inputs, labels in tqdm(train_loader, desc=f"Epoch {epoch+1}/{num_epochs}", unit="batch"):
+        for inputs, labels in tqdm(
+            train_loader, desc=f"Epoch {epoch+1}/{num_epochs}", unit="batch"
+        ):
             inputs, labels = inputs.to(device), labels.to(device)
 
             optimizer.zero_grad()
@@ -87,7 +95,9 @@ def train_model(model, train_loader, test_loader, criterion, optimizer, num_epoc
 
         epoch_loss = running_loss / total
         epoch_accuracy = correct / total
-        print(f"Epoch {epoch+1}/{num_epochs}, Loss: {epoch_loss:.4f}, Accuracy: {epoch_accuracy:.4f}")
+        print(
+            f"Epoch {epoch+1}/{num_epochs}, Loss: {epoch_loss:.4f}, Accuracy: {epoch_accuracy:.4f}"
+        )
 
         # 验证模型性能
         model.eval()
@@ -106,10 +116,13 @@ def train_model(model, train_loader, test_loader, criterion, optimizer, num_epoc
 
         test_loss /= total
         test_accuracy = correct / total
-        print(f"Validation Loss: {test_loss:.4f}, Validation Accuracy: {test_accuracy:.4f}")
+        print(
+            f"Validation Loss: {test_loss:.4f}, Validation Accuracy: {test_accuracy:.4f}"
+        )
+
 
 def get_loader(data, labels):
-    data = np.transpose(data, (3, 2, 0, 1)) # 变为 (168233, 3, 32, 32)
+    data = np.transpose(data, (3, 2, 0, 1))  # 变为 (168233, 3, 32, 32)
 
     data = torch.tensor(data, dtype=torch.float32)
     labels = torch.tensor(labels.squeeze(), dtype=torch.long)
@@ -125,11 +138,12 @@ def get_loader(data, labels):
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
     return train_loader, test_loader
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print("loading data...")
     HASYv2 = unpickle("./data/HASYv2")
-    data = np.array(HASYv2['data']) # (32, 32, 3, 168233)
-    labels = np.array(HASYv2['labels']) # (168233, 1)
+    data = np.array(HASYv2["data"])  # (32, 32, 3, 168233)
+    labels = np.array(HASYv2["labels"])  # (168233, 1)
     print("processing data...")
     train_loader, test_loader = get_loader(data, labels)
     # data = data[:, :, 0, :]
@@ -157,11 +171,11 @@ if __name__ == '__main__':
     model = model.to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
-    train_model(model, train_loader, test_loader, criterion, optimizer, num_epochs=2)
+    train_model(model, train_loader, test_loader, criterion, optimizer, num_epochs=10)
 
     current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
-    model_directory = './models'
-    model_filename = f'model_{current_time}.pth'
+    model_directory = "./models"
+    model_filename = f"model_{current_time}.pth"
     full_model_path = os.path.join(model_directory, model_filename)
     os.makedirs(model_directory, exist_ok=True)
     torch.save(model.state_dict(), full_model_path)
